f223d3334498fc6e8745d1fc3a3abe6680dd87ec
Khaled Necir <khaledannab@gmail.com>
2018-12-27 10:44:45 +0100

    Update kernel.mk

--- kernel.mk
+++ kernel.mk
@@ -13,8 +13,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
+LOCAL_PATH := kernel/honor/msm8937_64
 # Kernel
+
+TARGET_KERNEL_SOURCE := $(LOCAL_PATH)/kernel/honor/msm8937_64
+TARGET_KERNEL_CONFIG := merge_msm8937_64_defconfig
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci slub_min_objects=12 unmovable_isolate1=2:192M,3:224M,4:256M unmovable_isolate2=2:64M,3:80M,4:80M androidboot.selinux=enforcing androidboot.selinux=enforcing buildvariant=userdebug androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
